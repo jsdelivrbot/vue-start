e99f66edf69b42a2fe3451595ce27d605b55ea84
aleks <aleks.kinokus@gmail.com>
2017-09-25 16:44:08 +0300

    start

--- starMe/src/components/Tst.vue
+++ starMe/src/components/Tst.vue
@@ -5,17 +5,49 @@
     <span v-bind:title="title">
           Hover your mouse over me for a few seconds to see my dynamically bound title!
     </span>
+    
+    <br>
+    <button v-on:click="(e) => addTask(currentText,currentHours)">add task</button>
+    <button>send tasks</button>
+    <button>get tasks</button>
+    <br>
+    <input type="text" v-model="currentText">
+    <input type="text" v-model="currentHours">
+    
+    <table>
+    <tr v-for="todo in tasks">
+      <td class="text">{{ todo.text }}</td>
+      <td class="hours">{{ todo.hours }}</td>
+    </tr>
+
+    </table>
+  
+  
   </div>
 </template>
 
 <script>
   export default {
+//    el:'#tst',
     name: 'tst',
     data () {
       return {
+        
+        currentHours: 100500,
+        currentText: 'Learn VUE',
+        
         msg: 'This is a test message',
-        date: new Date(),
+        date: new Date().toLocaleString(),
         title: new Date(),
+        tasks: [{text: 'buy milk', hours: 5}, {text: 'buy cheese', hours: 5},]
+      }
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      },
+      addTask: function (text, hours) {
+        this.tasks.push({text: text, hours: hours})
       }
     }
   }
@@ -30,14 +62,25 @@
   ul {
     list-style-type: none;
     padding: 0;
+    text-align: left;
   }
   
   li {
-    display: inline-block;
+    display: block;
     margin: 0 10px;
+    text-align: left;
+    
   }
   
   a {
     color: #42b983;
+  }
+  
+  td.text{
+    text-align: left;
+  }
+  
+  td.hours{
+    text-align: right;
   }
 </style>
