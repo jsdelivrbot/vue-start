275654f8d7be5eec6dc96151693590ed6e352f7f
aleks <aleks.kinokus@gmail.com>
2017-09-26 16:03:19 +0300

    start

---
+++ starMe/src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="login">
+    <!--<img src="./assets/logo.png">-->
+    <input type="text">
+    <input type="text">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style>
+#login {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- starMe/src/main.js
+++ starMe/src/main.js
@@ -2,14 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Login from './Login'
 import router from './router'
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
-new Vue({
+let app = new Vue({
   el: '#app',
   router,
   template: '<App/>',
   components: { App }
+});
+
+let login = new Vue({
+  el: '#login',
+  router,
+  template: '<Login/>',
+  components: { Login }
 });
