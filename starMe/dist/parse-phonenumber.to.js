969f18ba683689fa42e7579fc12144eac0df0bdc
aleks <aleks.kinokus@gmail.com>
2017-12-25 17:05:32 +0200

    t

--- starMe/dist/parse-phonenumber.to.js
+++ starMe/dist/parse-phonenumber.to.js
@@ -53,13 +53,17 @@ let scrape = async () => {
     // 'https://yandex.ru/search/?text=host%3Aphonenumber.to&lr=130&site=phonenumber.to',
     // 'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D1%83%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0',
     // 'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D1%80%D0%BE%D1%81%D1%81%D0%B8%D1%8F',
-    'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D0%B2%D0%B0%D1%81%D1%8F&lr=130',
+    // 'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D0%B2%D0%B0%D1%81%D1%8F&lr=130',
+    'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D0%B2%D0%B0%D1%81%D0%B8%D0%BB%D0%B8%D0%B9&lr=130',
+    'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D0%BA%D0%BB%D1%83%D0%B1&lr=130',
+    'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D0%BA%D0%BE%D0%BB%D0%BB%D0%B5%D0%BA%D1%82%D0%BE%D1%80&lr=130',
   ];
   let yandexBrowserSearch = async (url, page) => {
     return new Promise(async function (resolve, reject) {
       let nextState = true;
       await page.goto(url);
       (await page.$$eval('a.link_theme_normal', items => items.map(item => item.href))).map(href => items.push(href));
+
       for (let i = 0; i <= yandexPages && nextState; i++) {
         (await page.$$eval('a.link_theme_normal', items => items.map(item => item.href))).map(href => items.push(href));
         try {
@@ -70,7 +74,7 @@ let scrape = async () => {
         await page.waitFor(3000);
         console.log(i, items.length);
       }
-      console.log(url,items.length);
+      console.log(url,items.length, items);
       resolve();
     });
 
