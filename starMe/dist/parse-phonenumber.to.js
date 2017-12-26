429fd05267b46a8556a034ae5219e5b52dea58eb
kinokus <aleks.kinokus@gmail.com>
2017-12-26 06:26:55 +0200

    ready

--- starMe/dist/parse-phonenumber.to.js
+++ starMe/dist/parse-phonenumber.to.js
@@ -54,9 +54,10 @@ let scrape = async () => {
     // 'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D1%83%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0',
     // 'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D1%80%D0%BE%D1%81%D1%81%D0%B8%D1%8F',
     // 'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D0%B2%D0%B0%D1%81%D1%8F&lr=130',
-    'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D0%B2%D0%B0%D1%81%D0%B8%D0%BB%D0%B8%D0%B9&lr=130',
-    'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D0%BA%D0%BB%D1%83%D0%B1&lr=130',
-    'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D0%BA%D0%BE%D0%BB%D0%BB%D0%B5%D0%BA%D1%82%D0%BE%D1%80&lr=130',
+    // 'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D0%B2%D0%B0%D1%81%D0%B8%D0%BB%D0%B8%D0%B9&lr=130',
+    // 'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D0%BA%D0%BB%D1%83%D0%B1&lr=130',
+    // 'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D0%BA%D0%BE%D0%BB%D0%BB%D0%B5%D0%BA%D1%82%D0%BE%D1%80&lr=130',
+    'https://yandex.ru/search/?text=host%3Aphonenumber.to%20%D1%80%D0%B8%D1%8D%D0%BB%D1%82%D0%BE%D1%80&lr=130',
   ];
   let yandexBrowserSearch = async (url, page) => {
     return new Promise(async function (resolve, reject) {
